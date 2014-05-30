f572fe31f02271fece7a18973fef3f7ecae2f7c1
jt <jt@nclouds.com>
2014-05-30 15:14:28 -0700

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'opsworks-testing'
+maintainer       'nclouds.com'
+maintainer_email 'jt@nclouds.com'
+license          'Apache 2.0'
+description      'Installs/Configures blah'
+long_description 'Installs/Configures blah'
+version          '0.1.0'
+
